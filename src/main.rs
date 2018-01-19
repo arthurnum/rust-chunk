6019f6253f84d768373f47082ea6ada52b33c90a
arthurnum <arthurnum@gmail.com>
2018-01-19 17:44:43 +0300

    fix program linking

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,11 @@ fn compile_shader(gl: &Gl, type_: GLenum, source: &[u8]) -> GLuint {
         println!("log length {:?}", *length);
         let s = String::from_raw_parts(log_ptr as *mut u8, *length as usize, 512);
         println!("{:?}", s);
+
+                println!("isShader {:?} -> {:?}", shader, gl.IsShader(shader));
         if compile_status == 0 {
-            shader
-        } else { 0 }
+            0
+        } else { shader }
     }
 }
 
@@ -46,11 +48,12 @@ fn vertex_shader(gl: &Gl) -> GLuint {
     let source = CString::new("
         #version 410 core
         layout(location=0) in vec3 pos;
-        // uniform mat4 MVP;
+
+        uniform mat4 supermatrix;
 
         void main()
         {
-            gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+            gl_Position = supermatrix * vec4(pos, 1.0);
         }
     ").unwrap();
     let sb = source.to_bytes();
@@ -78,6 +81,18 @@ fn create_program(gl: &Gl, vs: GLuint, fs: GLuint) -> GLuint {
         gl.AttachShader(program, vs);
         gl.AttachShader(program, fs);
         gl.LinkProgram(program);
+
+        let mut link_status: i32 = 0;
+        gl.GetShaderiv(program, LINK_STATUS, &mut link_status);
+        println!("link status: {:?}", link_status);
+        let mut log: [i8; 512] = [0; 512];
+        let log_ptr: *mut i8 = &mut log[0];
+        let length: *mut i32 = &mut 0;
+        gl.GetProgramInfoLog(program, 512, length, log_ptr);
+        println!("log length {:?}", *length);
+        let s = String::from_raw_parts(log_ptr as *mut u8, *length as usize, 512);
+        println!("{:?}", s);
+
         program
     }
 }
@@ -99,15 +114,11 @@ fn draw_rectangle_sample(gl: &Gl) -> GLuint {
             110.0, 110.0, 1.0,
         ];
 
-        // let vptr = &vertices as *const _ as *const std::os::raw::c_void;
-
-        // gl.BufferData(ARRAY_BUFFER, 4*12, vptr, STATIC_DRAW);
         gl.BufferData(ARRAY_BUFFER, 4*12, std::mem::transmute(&vertices[0]), STATIC_DRAW);
 
         gl.EnableVertexAttribArray(0);
         gl.VertexAttribPointer(0, 3, FLOAT, FALSE, 0, std::ptr::null());
         vao
-        // gl.DrawArrays(TRIANGLE_STRIP, 0, 4);
     }
 }
 
@@ -118,7 +129,11 @@ fn main() {
 
     let video_subsys = sdl_context.video().unwrap();
 
-    let window = video_subsys.window("Title", 600, 400).opengl().build().unwrap();
+    let window = video_subsys.window("Title", 600, 400)
+                             .opengl()
+                             .resizable()
+                             .build()
+                             .unwrap();
 
     video_subsys.gl_attr().set_context_profile(sdl2::video::GLProfile::Core);
     video_subsys.gl_attr().set_double_buffer(true);
@@ -142,12 +157,14 @@ fn main() {
     // let mat:Matrix4<f32> = cgmath::ortho(0.0, 600.0, 0.0, 400.0, -1.0, 1.0).into();
     // let orthomatrix = cgmath::ortho(0.0, 600.0, 0.0, 400.0, -1.0, 1.0);
     // println!("{:?}", ortho2d(0.0, 600.0, 0.0, 400.0));
-    // println!("{:?}", orthomatrix);
-    //
+    println!("{:?}", orthomatrix);
+
     let vs = vertex_shader(&gl);
     let fs = fragment_shader(&gl);
     let program = create_program(&gl, vs, fs);
+    println!("Program {:?}", program);
     let vao = draw_rectangle_sample(&gl);
+    println!("VAO {:?}", vao);
 
     let mut exit = false;
 
@@ -158,11 +175,10 @@ fn main() {
             gl.Clear(COLOR_BUFFER_BIT);
 
             gl.UseProgram(program);
-            let uname = CString::new("MVP").unwrap();
+            let uname = CString::new("supermatrix").unwrap();
             let ul = gl.GetUniformLocation(program, uname.as_ptr());
             gl.UniformMatrix4fv(ul, 1, FALSE, orthomatrix.as_ptr());
 
-            draw_rectangle_sample(&gl);
             gl.BindVertexArray(vao);
             gl.DrawArrays(TRIANGLE_STRIP, 0, 4);
         }
