678435161c5ef053289660346c622a2cafc6aa92
arthurnum <arthurnum@gmail.com>
2018-02-01 16:09:48 +0300

    refactor timers

--- src/timers.rs
+++ src/timers.rs
@@ -1,24 +1,24 @@
-use time;
+use time::{SteadyTime};
 
 pub struct Timer {
-    start: time::SteadyTime,
-    last_frame: time::SteadyTime
+    start: SteadyTime,
+    last_frame: SteadyTime
 }
 
 pub fn new() -> Box<Timer> {
-    Box::new(Timer { start: time::SteadyTime::now(),
-                     last_frame: time::SteadyTime::now()})
+    Box::new(Timer { start: SteadyTime::now(),
+                     last_frame: SteadyTime::now()})
 }
 
 impl Timer {
     pub fn elapsed(&self) -> i64 {
-        let dur = time::SteadyTime::now() - self.start;
+        let dur = SteadyTime::now() - self.start;
         dur.num_milliseconds()
     }
 
     pub fn frame_time(&mut self) -> i64 {
         let origin = self.last_frame;
-        self.last_frame = time::SteadyTime::now();
+        self.last_frame = SteadyTime::now();
         (self.last_frame - origin).num_milliseconds()
     }
 }
